ec3711194eb39aa1e0a0d61b082decda9b85ba3b
Amol Patel <amolpatel4@gmail.com>
2017-12-23 09:24:30 -0500

    adding directories for containers and components. added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { getPosts, savePost, deletePost } from './actions/PostActions';
--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers/index'
 import { Provider } from 'react-redux'
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <App />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={App}/>
+        </Switch>
+      </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
@@ -19,3 +23,6 @@ registerServiceWorker();
 
 // created a store (app state) and applied middleware of thunk to the store
 // then applied all of the reducers
+
+// BROWSER ROUTER
+// applies most applicable first so home needs to go at bottom
